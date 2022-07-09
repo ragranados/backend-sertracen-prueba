960831e15f661f8326f881c29d9a44eb75477ebe
Raúl Granados <raul.granados12@gmail.com>
2022-07-08 23:24:42 -0600

    Moviendo cors

--- app.js
+++ app.js
@@ -11,7 +11,6 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
-app.use(cors);
 
 // view engine setup
 
@@ -26,6 +25,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+app.use(cors);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
