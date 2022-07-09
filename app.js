b49b619522090e827561a9f15600e347d731d05d
Raúl Granados <raul.granados12@gmail.com>
2022-07-08 23:19:29 -0600

    Agregando cors

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 var createError = require('http-errors');
 var express = require('express');
+const cors = require('cors');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
@@ -10,6 +11,7 @@ var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
 var app = express();
+app.use(cors);
 
 // view engine setup
 
